b8c8883e2fe1b1f124ad99a23dc485bd1ab44d14
Khanh <knh.nguyen2002@gmail.com>
2023-06-27 16:39:33 -0500

    Added video slider section to HTML and CSS

--- javascript.js
+++ javascript.js
@@ -76,3 +76,18 @@ email.addEventListener("click", function() {
         My Email: knh.nguyen2002@gmail.com`
     }
 })
+
+// video slider
+const swiper = new Swiper('.swiper', {
+	loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
